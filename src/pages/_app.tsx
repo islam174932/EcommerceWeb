4984f9bd4ebf2b346929cd76f419e2e220860150
islam174932 <eslamgamal19933@gmail.com>
2025-09-17 12:18:47 +0200

    Resolve merge conflict markers and fix _app.tsx syntax for Vercel build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 694477f3e53c5d5237ad8cd87b49f6e6c00fa475
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-<<<<<<< HEAD
+
 }
-=======
-}}
->>>>>>> 694477f3e53c5d5237ad8cd87b49f6e6c00fa475
